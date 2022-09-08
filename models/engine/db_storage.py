8fa6f0ae536b2db6f249b0e4bd00cf42e08e3913
Nachop51 <nachoperalta0@gmail.com>
2022-09-08 16:57:39 +0000

    adding new methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,18 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ Returns the object based on the class and its ID """
+        if cls and id:
+            return self.__session.query(cls).filter_by(id=id).first()
+        return None
+    
+    def count(self, cls=None):
+        """ Returns the number of objects in storage """
+        if cls:
+            return self.__session.query(cls).count()
+        count = 0
+        for clss in classes.values():
+            count += self.__session.query(clss).count()
+        return count
