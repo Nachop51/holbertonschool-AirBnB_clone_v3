5ca7c9f1988d122ce3a4aa71736e4f0e7c542b80
Nachop51 <nachoperalta0@gmail.com>
2022-09-09 02:32:48 +0000

    fixing indentation

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,15 +10,16 @@ from models.state import State
 def states():
     ''' retrieves the number of each objects by type '''
     if request.method == 'GET':
-      return jsonify([obj.to_dict() for obj in storage.all("State").values()])
+        return jsonify([obj.to_dict() for obj in
+                        storage.all("State").values()])
     elif request.method == 'POST':
-      if not request.get_json():
-        abort(400, 'Not a JSON')
-      if 'name' not in request.get_json():
-        abort(400, 'Missing name')
-      new_state = State(**request.get_json())
-      new_state.save()
-      return jsonify(new_state.to_dict()), 201
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+        if 'name' not in request.get_json():
+            abort(400, 'Missing name')
+        new_state = State(**request.get_json())
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
@@ -27,21 +28,21 @@ def state(state_id):
     state_list = [obj.to_dict() for obj in storage.all("State").values()]
     ids = [obj['id'] for obj in state_list]
     if state_id in ids:
-      if request.method == 'GET':
-        return jsonify(state_list[ids.index(state_id)])
-      elif request.method == 'DELETE':
-        storage.delete(storage.get(State, state_id))
-        storage.save()
-        return {}, 200
-      elif request.method == 'PUT':
-        if not request.get_json():
-          abort(400, 'Not a JSON')
+        if request.method == 'GET':
+            return jsonify(state_list[ids.index(state_id)])
+        elif request.method == 'DELETE':
+            storage.delete(storage.get(State, state_id))
+            storage.save()
+            return {}, 200
+        elif request.method == 'PUT':
+            if not request.get_json():
+                abort(400, 'Not a JSON')
         else:
-          state = storage.get(State, state_id)
-          for key, value in request.get_json().items():
-            if key not in ['id', 'created_at', 'updated_at']:
-              setattr(state, key, value)
-          storage.save()
-          return jsonify(state.to_dict()), 200
+            state = storage.get(State, state_id)
+            for key, value in request.get_json().items():
+                if key not in ['id', 'created_at', 'updated_at']:
+                    setattr(state, key, value)
+            storage.save()
+            return jsonify(state.to_dict()), 200
     else:
-      abort(404)
+        abort(404)
