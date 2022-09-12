d21d61922c90ea7e9c39ef84c40391a0c459c1a4
Nachop51 <nachoperalta0@gmail.com>
2022-09-12 00:15:19 +0000

    removing extra lines of code

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -40,10 +40,6 @@ def user(user_id):
         elif request.method == "PUT":
             if not request.get_json():
                 abort(400, 'Not a JSON')
-            elif 'email' not in request.get_json():
-                abort(400, "Mising email")
-            elif 'password' not in request.get_json():
-                abort(400, 'Missing password')
             else:
                 user = storage.get(User, user_id)
                 for key, value in request.get_json().items():
