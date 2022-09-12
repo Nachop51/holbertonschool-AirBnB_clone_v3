1ec24f163687df73b3b8f51ee61a2f7eac81081b
Nachop51 <nachoperalta0@gmail.com>
2022-09-12 00:45:36 +0000

    missing import

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,7 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.place import Place
 from models.user import User
 from models.review import Review
 
